d0b3cab4ecca4e0d1a096d4fd9c4e245832d78eb
Aakash Sangapure <aakashdadasangapure@gmail.com>
2022-12-27 18:26:07 +0530

    implemented search in  backend

--- models/Property.js
+++ models/Property.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 const PropertySchema= new mongoose.Schema({
-
+    PPDID:{type:String, unique:true},
     propertyType:{
         type:String,
         enum:['Home','Plot','Flat']
--- routes/property.js
+++ routes/property.js
@@ -4,6 +4,7 @@ const Property= require("../models/Property");
 
 const router= express.Router();
 
+
 router.get("/",async(req,res)=>{
     try{
         const data = await Property.find();
@@ -15,12 +16,24 @@ router.get("/",async(req,res)=>{
 })
 
 
-
+router.get("/search",async(req,res)=>{ 
+    let pattern= new RegExp("^"+req.body.search)    
+    let property = await Property.find({PPDID:{$regex:pattern}})
+    res.json({property})
+})
 
 router.post("/addProperty",isAuthenticated, async(req,res)=>{
     try{
         // const {propertyType,negotiable, price, ownership} = req.body;
-        let  property = await Property.create(req.body);
+        let PPDID="PPD"+ Math.floor(1000 + Math.random() * 9000)
+        for(let i=0;;i++){
+            let prop = await Property.findOne({PPDID})
+            if(prop){
+                continue
+            }
+            else break
+        }
+        let  property = await Property.create({...req.body,PPDID});
         res.json({property, message:"Property Added Succesfully"})
         
     }
@@ -28,5 +41,8 @@ router.post("/addProperty",isAuthenticated, async(req,res)=>{
         res.json({e});
     }
 });
+
+
+
 
 module.exports = router;
